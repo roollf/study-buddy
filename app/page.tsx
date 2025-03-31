40d6e852b1613339529a55e5d7da2f21d38978e1
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-03-31 16:17:30 -0300

    :art: chore: adjust main layout padding in Home component and remove unused Table import

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 // import components
 import Card from "./_components/card/card";
-import Table from "./_components/table/table";
 import Chart from "./_components/chart/chart";
 
 export default function Home() {
   return (
-    <main className="flex flex-col md:flex-row items-center justify-center h-full gap-10 md:gap-20 mx-auto my-auto">
+    <main className="flex py-5 md:py-0 flex-col md:flex-row items-center justify-center h-full gap-10 md:gap-20 mx-auto my-auto">
       <Card />
-      {/* <Table /> */}
       <Chart />
     </main>
   );
