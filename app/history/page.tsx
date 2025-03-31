dea4b4392317de71798f3c4a3e13185f00c734ef
Rolf Matela <rolffreitas@yahoo.com.br>
2025-03-31 20:53:56 -0300

    :recycle: refactor: simplify History component by removing unnecessary wrapper around Table

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -4,9 +4,7 @@ import Table from "../_components/table/table";
 export default function History() {
   return (
     <main className="flex flex-col md:flex-row items-center justify-center gap-10 md:gap-20 mx-auto my-auto">
-      <h1>
-        <Table />
-      </h1>
+      <Table />
     </main>
   );
 }
