ffc8a0ee0427b1c68b36d2b1a6c67262aef9d7ba
Rolf Matela <rolffreitas@yahoo.com.br>
2025-03-31 11:02:34 -0300

    :sparkles: feat: add mock data for legal disciplines and topics

---
+++ app/_mock/data.ts
@@ -0,0 +1,28 @@
+const data = [
+  {
+    disciplina: "Direito Penal",
+    tema: "Culpabilidade",
+  },
+  {
+    disciplina: "Direito Constitucional",
+    tema: "Princípios Fundamentais",
+  },
+  {
+    disciplina: "Direito Civil",
+    tema: "Teoria Geral dos Contratos",
+  },
+  {
+    disciplina: "Direito do Trabalho",
+    tema: "Contrato de Trabalho",
+  },
+  {
+    disciplina: "Direito Tributário",
+    tema: "Imunidade Tributária",
+  },
+  {
+    disciplina: "Direito Ambiental",
+    tema: "Princípio do Desenvolvimento Sustentável",
+  },
+];
+
+export default data;
