b79f9d3f16c2257bab90e0fc9cda940630eb05bb
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-03-31 16:16:39 -0300

    :sparkles: feat: add History page with Table component integration

---
+++ app/history/page.tsx
@@ -0,0 +1,12 @@
+// import components
+import Table from "../_components/table/table";
+
+export default function History() {
+  return (
+    <main className="flex flex-col md:flex-row items-center justify-center gap-10 md:gap-20 mx-auto my-auto">
+      <h1>
+        <Table />
+      </h1>
+    </main>
+  );
+}
