5160eb0dcabfdab1a34586e19e33ef40f4ea88ec
Rolf Freitas Matela <rolffreitas@yahoo.com.br>
2025-03-31 16:19:07 -0300

    :recycle: refactor: enhance Table component with no data state and total duration display

--- app/_components/table/table.tsx
+++ app/_components/table/table.tsx
@@ -6,6 +6,9 @@ import * as TableComponent from "@/components/ui/table";
 // import context
 import { useTimer } from "@/app/_context/timerContext";
 
+// import utils
+import formatTime from "@/app/_utils/formatTime";
+
 export default function Table() {
   const { selectedDataList } = useTimer();
 
@@ -21,36 +24,69 @@ export default function Table() {
     };
   });
 
+  const totalDuration = selectedDataList.reduce((acc, item) => {
+    return acc + item.duration;
+  }, 0);
+
   return (
-    <TableComponent.Table className="w-full max-w-3xl mx-auto">
-      <TableComponent.TableHeader>
-        <TableComponent.TableRow>
-          <TableComponent.TableHead className="w-[100px]">
-            Matéria
-          </TableComponent.TableHead>
-          <TableComponent.TableHead className="w-[100px]">
-            Tema
-          </TableComponent.TableHead>
-          <TableComponent.TableHead className="w-[100px] text-right">
-            Tempo
-          </TableComponent.TableHead>
-        </TableComponent.TableRow>
-      </TableComponent.TableHeader>
-      <TableComponent.TableBody>
-        {data.map((item, index) => (
-          <TableComponent.TableRow key={index}>
-            <TableComponent.TableCell className="w-[100px] font-semibold">
-              {item.disciplina}
-            </TableComponent.TableCell>
-            <TableComponent.TableCell className="w-[100px]">
-              {item.tema}
-            </TableComponent.TableCell>
-            <TableComponent.TableCell className="w-[100px] text-right">
-              {item.duration}
-            </TableComponent.TableCell>
-          </TableComponent.TableRow>
-        ))}
-      </TableComponent.TableBody>
-    </TableComponent.Table>
+    <>
+      {selectedDataList.length === 0 ? (
+        <div className="flex flex-col items-center justify-center gap-4">
+          <p className="text-zinc-800 text-[16px] font-semibold">
+            Você ainda não estudou nada.
+          </p>
+          <p className="text-zinc-500 text-[12px]">
+            Assim que você estudar algo, ele aparecerá aqui.
+          </p>
+        </div>
+      ) : (
+        <TableComponent.Table className="md:w-[800px] max-w-3xl mx-auto">
+          <TableComponent.TableCaption>
+            Uma lista com as matérias e temas que você estudou.
+          </TableComponent.TableCaption>
+          <TableComponent.TableHeader>
+            <TableComponent.TableRow>
+              <TableComponent.TableHead className="w-[100px] text-zinc-800 text-[16px] font-bold">
+                Matéria
+              </TableComponent.TableHead>
+              <TableComponent.TableHead className="w-[100px] text-zinc-800 text-[16px] font-bold ">
+                Tema
+              </TableComponent.TableHead>
+              <TableComponent.TableHead className="w-[100px] text-zinc-800 text-[16px] font-bold text-right">
+                Tempo
+              </TableComponent.TableHead>
+            </TableComponent.TableRow>
+          </TableComponent.TableHeader>
+          <TableComponent.TableBody>
+            {data.map((item, index) => (
+              <TableComponent.TableRow key={index}>
+                <TableComponent.TableCell className="w-[100px] text-[12px] text-zinc-800 font-semibold">
+                  {item.disciplina}
+                </TableComponent.TableCell>
+                <TableComponent.TableCell className="w-[100px] text-[12px] text-zinc-500">
+                  {item.tema}
+                </TableComponent.TableCell>
+                <TableComponent.TableCell className="w-[100px] text-[12px] text-right">
+                  {item.duration}
+                </TableComponent.TableCell>
+              </TableComponent.TableRow>
+            ))}
+          </TableComponent.TableBody>
+          <TableComponent.TableFooter>
+            <TableComponent.TableRow>
+              <TableComponent.TableCell
+                colSpan={2}
+                className="text-zinc-800 font-semibold"
+              >
+                Total
+              </TableComponent.TableCell>
+              <TableComponent.TableCell className="text-[12px] text-zinc-800 font-semibold text-right">
+                {formatTime(totalDuration)}
+              </TableComponent.TableCell>
+            </TableComponent.TableRow>
+          </TableComponent.TableFooter>
+        </TableComponent.Table>
+      )}
+    </>
   );
 }
